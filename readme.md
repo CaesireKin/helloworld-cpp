9cf16585cc4d36845a551c593f95689e9413590c
jon <me@caesirekin.me>
2019-05-03 14:55:06 +0800

    Update

--- readme.md
+++ readme.md
@@ -2,3 +2,25 @@
 
 This is a hello world program written by C++ \
 The only purpose is to print the basic types provide by C++
+
+## Outputs
+
+In 64bit OS
+```
+This is a type test writen by jon
+sizeof(char)1
+sizeof(bool)1
+sizeof(short)2
+sizeof(int)4
+sizeof(long)8
+sizeof(float)4
+sizeof(double)8
+
+sizeof(char*)8
+sizeof(bool*)8
+sizeof(short*)8
+sizeof(int*)8
+sizeof(long*)8
+sizeof(float*)8
+sizeof(double*)8
+```
--- src/hello-world.cpp
+++ src/hello-world.cpp
@@ -5,9 +5,17 @@ using namespace std;
 int main(int argc, char const* argv[]) {
     cout << "This is a type test writen by jon" << endl
         << "sizeof(char)" << sizeof(char) << endl
+        << "sizeof(bool)" << sizeof(bool) << endl
         << "sizeof(short)" << sizeof(short) << endl
         << "sizeof(int)" << sizeof(int) << endl
         << "sizeof(long)" << sizeof(long) << endl
         << "sizeof(float)" << sizeof(float) << endl
-        << "sizeof(double)" << sizeof(double) << endl;
+        << "sizeof(double)" << sizeof(double) << endl << endl
+        << "sizeof(char*)" << sizeof(char*) << endl
+        << "sizeof(bool*)" << sizeof(bool*) << endl
+        << "sizeof(short*)" << sizeof(short*) << endl
+        << "sizeof(int*)" << sizeof(int*) << endl
+        << "sizeof(long*)" << sizeof(long*) << endl
+        << "sizeof(float*)" << sizeof(float*) << endl
+        << "sizeof(double*)" << sizeof(double*) << endl;
 }
